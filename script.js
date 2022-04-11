ec50e425abebce3b5e1d9b5f7e3a9c97ec3daa41
dima12345678qaz <dmitrora93@gmail.com>
2022-04-11 13:58:37 +0200

    add home work

---
+++ script.js
@@ -0,0 +1,59 @@
+
+//task1
+// let elem1=document.getElementsByTagName('div')[0].innerHTML='Last';
+// let elem=document.getElementById('test').innerHTML='Last';
+
+
+
+//task2
+
+// let img=document.getElementsByClassName('image')[0].src='cat.jpg';
+// alert(document.getElementsByClassName('image')[0].src);
+
+
+
+//task3
+
+// let allElement=[...document.querySelectorAll('div p')]
+// for (let i = 0; i < allElement.length; i++) {
+//     console.log(`Selector text ${i}: ${allElement[i].innerHTML}  `)
+    
+// }
+
+
+//task4
+// let list=[...document.getElementsByTagName('li')];
+// console.log(list[0].innerHTML);
+// console.log(list[list.length-1].innerHTML);
+// console.log(list[1].innerHTML);
+// console.log(list[list.length-2].innerHTML);
+// console.log(list[list.length-3].innerHTML);
+
+//task5
+
+// document.getElementsByTagName('h1')[0].style.background='green';
+// document.getElementsByTagName('p')[0].style.fontWeight='bold'
+// document.getElementsByTagName('span')[0].style.display='none';
+// document.getElementById('myList').style.display='flex';
+// document.getElementById('myList').style.listStyle='none'
+
+//task6
+
+// let a=prompt('first input')
+// let b=prompt('second input')
+// document.getElementById('input1').value=a;
+// document.getElementById('input2').value=b;
+// document.getElementById('input1').value=b;
+// document.getElementById('input2').value=a;
+
+//task7
+
+// let main=document.createElement('main');
+// main.classList.add('mainClass',  'check', 'item');
+// document.body.append(main);
+// let div=document.createElement('div');
+// main.append(div);
+// div.id='myDiv';
+// let p=document.createElement('p')
+// p.innerHTML='First paragraph';
+// div.append(p);
